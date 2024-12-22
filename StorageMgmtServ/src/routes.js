a1fb799c1af9e26e2edcd39faef7e7a268142909
Umar Farooq <140904339+Umar-Farooq-2112@users.noreply.github.com>
2024-12-22 17:19:54 +0500

    basic changes

--- StorageMgmtServ/src/routes.js
+++ StorageMgmtServ/src/routes.js
@@ -3,10 +3,14 @@ const router = Router();
 const mysqlConnection = require('./database');
 
 
-
 router.get('/', (req, res) => {
     res.status(200).json('Server running and Database is connected.');
 });
 
-router.get('')
+router.post('/upload-video',(req,res)=>{
+    res.json({status:"OK"});
 
+
+});
+
+module.exports = router;
